09551de30a25dc26b49b91f7a3022f609491f741
jimchen5209 <jimchen5209@gmail.com>
2025-10-01 13:34:23 +0800

    build: update com.gradleup.shadow  `8.3.9` -> `9.2.2`

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `maven-publish`
     kotlin("jvm") version "2.2.20"
-    id("com.gradleup.shadow") version "8.3.9"
+    id("com.gradleup.shadow") version "9.2.2"
 }
 
 group = "one.oktw"
